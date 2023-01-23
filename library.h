e4d7c0c20bbce16e2dd37fd90dc837b478c15a1d
r5330 <119348084+r5330@users.noreply.github.com>
2023-01-23 23:36:09 +0100

    Add files via upload

---
+++ library.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
+#include <fstream>
+#include <list>
+#include <deque>
+
+using std::ifstream;
+using std::string;
+using std::vector;
+using std::ostream;
+using std::istream;
+using std::cout;
+using std::cin;
+using std::istringstream;
+using std::fixed;
+using std::setprecision;
+using std::setw;
+using std::left;
+using std::list;
+using std::deque;
+
+#include "student.h"
