7f131236e996c687935d7d42fd2c7d5bd9ea4ed1
r5330 <119348084+r5330@users.noreply.github.com>
2023-01-23 23:34:23 +0100

    Add files via upload

---
+++ student.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "library.h"
+//int output_option;
+
+class Student {
+    string vardas, pavarde;
+    vector<int> nd;
+    list<int> nd_l;
+    deque<int> nd_d;
+    int egz;
+    double galutinis;
+public:
+    Student();
+    Student(const Student& C);
+    Student& operator=(const Student&) {
+        return *this;
+    }
+    Student(string, string, list<int>, int);
+    Student(string, string, deque<int>, int);
+    Student(string, string, vector<int>, int);
+    friend istream& operator>>(istream &input, Student &S);
+    friend ostream& operator<<(ostream &output, const Student &S);
+    void calcGalutinis(int);
+    bool operator <(Student& b) {
+        if (this->getPavarde() < b.getPavarde()) return true;
+        else return false;
+    }
+    string getVardas();
+    string getPavarde();
+    ~Student(){}
+};
